3f18154079a7af38fe74ca8261fb31a1765dd5cd
Empix <jotinhave@gmail.com>
2021-01-29 15:03:15 -0400

    Remove code that adds syllables

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-const toAdd = ['á_', 'â_', 'é_', 'ê_', 'i_', 'ó_', 'ô_', 'u_']; // vogais para adicionar
-const bidi = ['a', 'a', 'e', 'e', 'i', 'o', 'o', 'u']; // correspondencia em bidi (repete por conta da posição)
-const vowels = ['á', 'â', 'é', 'ê', 'í', 'ó', 'ô', 'ú']; // i, u com acento
-const vowels2 = ['á', 'â', 'é', 'ê', 'i', 'ó', 'ô', 'u']; // i, u sem acento
-const AllVowels = ['á', 'â', 'é', 'ê', 'i', 'í', 'ó', 'ô', 'u', 'ú'];
-const AllVowelsBidi = ['á', 'â', 'é', 'ê', 'í', 'í', 'ó', 'ô', 'ú', 'ú'];
-const consonants = [
-  'b_',
-  'c_',
-  'd_',
-  'f_',
-  'g_',
-  'h_',
-  'j_',
-  'k_',
-  'l_',
-  'm_',
-  'n_',
-  'p_',
-  'q_',
-  'r_',
-  's_',
-  't_',
-  'v_',
-  'w_',
-  'x_',
-  'y_',
-  'z_',
-  'ç_',
-]; // consoantes para adicionar
+// const toAdd = ['á_', 'â_', 'é_', 'ê_', 'i_', 'ó_', 'ô_', 'u_']; // vogais para adicionar
+// const bidi = ['a', 'a', 'e', 'e', 'i', 'o', 'o', 'u']; // correspondencia em bidi (repete por conta da posição)
+// const vowels = ['á', 'â', 'é', 'ê', 'í', 'ó', 'ô', 'ú']; // i, u com acento
+// const vowels2 = ['á', 'â', 'é', 'ê', 'i', 'ó', 'ô', 'u']; // i, u sem acento
+// const AllVowels = ['á', 'â', 'é', 'ê', 'i', 'í', 'ó', 'ô', 'u', 'ú'];
+// const AllVowelsBidi = ['á', 'â', 'é', 'ê', 'í', 'í', 'ó', 'ô', 'ú', 'ú'];
+// const consonants = [
+//   'b_',
+//   'c_',
+//   'd_',
+//   'f_',
+//   'g_',
+//   'h_',
+//   'j_',
+//   'k_',
+//   'l_',
+//   'm_',
+//   'n_',
+//   'p_',
+//   'q_',
+//   'r_',
+//   's_',
+//   't_',
+//   'v_',
+//   'w_',
+//   'x_',
+//   'y_',
+//   'z_',
+//   'ç_',
+// ]; // consoantes para adicionar
+
+// consonants.forEach((set) => {
+//   AllVowels.forEach((vowel, indexVowel) => {
+//     data[set.replace('_', vowel)] = `${set.replace('_', '')}${
+//       AllVowelsBidi[indexVowel]
+//     }`;
+//   });
+// }); // Adiciona as consoantes com as vogais
+
+// consonants.forEach((set) => {
+//   data[set.replace('_', '')] = set.replace('_', '');
+// }); // Adicionar as consoantes sozinhas
+
+// toAdd.forEach((set, index) => {
+//   AllVowels.forEach((vowel, indexVowel) => {
+//     data[
+//       set.replace('_', vowel)
+//     ] = `${bidi[index]}${AllVowelsBidi[indexVowel]}`;
+//   });
+// }); // Adiciona as vogais com as vogais
+
+// vowels2.forEach((vowel, index) => {
+//   data[vowel] = bidi[index];
+// }); // Adiciona as vogais sozinhas
 
 data['\n'] = '\n';
-
-consonants.forEach((set) => {
-  AllVowels.forEach((vowel, indexVowel) => {
-    data[set.replace('_', vowel)] = `${set.replace('_', '')}${
-      AllVowelsBidi[indexVowel]
-    }`;
-  });
-}); // Adiciona as consoantes com as vogais
-
-consonants.forEach((set) => {
-  data[set.replace('_', '')] = set.replace('_', '');
-}); // Adicionar as consoantes sozinhas
-
-toAdd.forEach((set, index) => {
-  AllVowels.forEach((vowel, indexVowel) => {
-    data[
-      set.replace('_', vowel)
-    ] = `${bidi[index]}${AllVowelsBidi[indexVowel]}`;
-  });
-}); // Adiciona as vogais com as vogais
-
-vowels2.forEach((vowel, index) => {
-  data[vowel] = bidi[index];
-}); // Adiciona as vogais sozinhas
 
 /**
  *
